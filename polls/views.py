bda1d945f2882cb593c08479076fc711dd4322a5
posmodernismoSIG <posmodernismo.sig@gmail.com>
2022-02-21 22:00:36 -0500

    Primeros templates basicos

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):                                                                     #Vista basada en Funcion recibe request
-    return HttpResponse('Estas en la pagina principal de premios platzi app')           #responde con un http
+    lastest_quiestion_list = Question.objects.all()
+    return render(request, "polls/index.html", 
+                  {"lastest_quiestion_list": lastest_quiestion_list})           #responde con un http
+
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta No {question_id}")
+    question =  get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question" : question})
 
 def resoult(request, question_id):
     return HttpResponse(f"Estas viendo los resultados de la pregunta No {question_id}")
